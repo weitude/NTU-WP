3dc5660d2ba91253595b658eeaf0f1914e0eca21
weitude <eric.c.w.lin@gmail.com>
2022-10-05 08:50:16 +0800

    basic done!

--- hw3/src/index.js
+++ hw3/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 
@@ -26,14 +26,18 @@ class TodoApp extends React.Component {
                             className="todo-app__input"
                             onChange={this.handleChange}
                             value={this.state.text}
+                            placeholder="What needs to be done?"
                         />
                     </form>
                     <TodoList items={this.state.items}/>
                 </div>
-                <footer className="todo-app__footer" id="todo-footer">
-                    <div className="todo-app__total">2 left</div>
+                <footer
+                    className="todo-app__footer"
+                    id="todo-footer"
+                    style={{display: this.state.items.length ? "" : "none"}}
+                >
+                    <div className="todo-app__total">{this.state.items.length} left</div>
                     <ul className="todo-app__view-buttons">
-
                         <li>
                             <button>All</button>
                         </li>
@@ -65,7 +69,7 @@ class TodoApp extends React.Component {
         }
         const newItem = {
             text: this.state.text,
-            id: Date.now()
+            id: Date.now(),
         };
         this.setState(state => ({
             items: state.items.concat(newItem),
@@ -75,23 +79,60 @@ class TodoApp extends React.Component {
 }
 
 class TodoList extends React.Component {
+
     render()
     {
         return (
-            <ul className="todo-app__list" id="todo-list">
-                {this.props.items.map(item => (
-                    <li className="todo-app__item" key={item.id}>
 
-                        <div className="todo-app__checkbox">
-                            <input type="checkbox" id="2"/>
-                            <label htmlFor="2"></label>
-                        </div>
-                        <h1 className="todo-app__item-detail">{item.text}</h1>
-                        <img src="https://i.imgur.com/JbhwBqT.png" className="todo-app__item-x" alt="item-x"/>
-                    </li>
+            <ul
+                className="todo-app__list"
+                id="todo-list"
+                style={{display: this.props.items.length ? "" : "none"}}
+            >
+                {this.props.items.map(item => (
+                    <TodoItem key={item.id} items={item}/>
                 ))}
             </ul>
         );
+    }
+}
+
+class TodoItem extends React.Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {complete: false};
+    }
+
+    change()
+    {
+        this.setState({complete: !this.state.complete});
+    }
+
+    render()
+    {
+        return (
+
+            <li className="todo-app__item">
+
+                <div className="todo-app__checkbox">
+                    <input
+                        type="checkbox"
+                        id={this.props.items.id}
+                        onClick={() => this.change()}
+                    />
+                    <label htmlFor={this.props.items.id}></label>
+                </div>
+                <h1
+                    className="todo-app__item-detail"
+                    style={{
+                        textDecoration: this.state.complete ? "line-through" : "",
+                        opacity: this.state.complete ? 0.5 : 1
+                    }}>{this.props.items.text}</h1>
+                <img src="https://i.imgur.com/JbhwBqT.png" className="todo-app__item-x" alt="item-x"/>
+            </li>
+        )
+
     }
 }
 
