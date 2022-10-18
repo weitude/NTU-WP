5074f19d565c4d7a7b0f1accf5f7403bcc585939
weitude <eric.c.w.lin@gmail.com>
2022-10-18 12:29:29 +0800

    s

--- hack1/src/components/Row.js
+++ hack1/src/components/Row.js
@@ -12,37 +12,31 @@ import React from 'react';
 
 const Row = ({guess, rowIdx}) =>
 {
-    return (
-        <div className='Row-container'>
-            {/* TODO 3: Row Implementation -- Row */}
-
-            {/* ↓ Default row, you should modify it. ↓ */}
-            {guess ?
-
-                <div className='Row-wrapper'>
+    if (guess)
+    {
+        return (
+            <div className='Row-wrapper'>
                     {guess.map((row, x) => (
-                        <div
-                            id={rowIdx + "-" + x}
-                            key={rowIdx + "-" + x}
-                            className={"Row-wordbox " + row.color}
-                        >
-                            {row.char}
-                        </div>
-                    ))}
-
-                </div> :
-                <div className='Row-wrapper'>
-
+                            <div
+                                id={rowIdx + "-" + x}
+                                key={rowIdx + "-" + x}
+                                className={"Row-wordbox " + row.color}
+                            >
+                                {row.char}
+                            </div>
+                        ))}
+            </div>)
+    }
+    else
+    return (
+        <div className='Row-wrapper'>
                     <div id={rowIdx + "-0"} key={rowIdx + "-0"} className={"Row-wordbox"}></div>
                     <div id={rowIdx + "-1"} key={rowIdx + "-1"} className={"Row-wordbox"}></div>
                     <div id={rowIdx + "-2"} key={rowIdx + "-2"} className={"Row-wordbox"}></div>
                     <div id={rowIdx + "-3"} key={rowIdx + "-3"} className={"Row-wordbox"}></div>
                     <div id={rowIdx + "-4"} key={rowIdx + "-4"} className={"Row-wordbox"}></div>
-                </div>
-
-            }
-            {/* ↑ Default row, you should modify it. ↑ */}
         </div>
+
     )
 }
 
