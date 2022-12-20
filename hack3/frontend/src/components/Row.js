b94d1c5f32cbaa29eab3aaaf5895cd072505f0e1
weitude <eric.c.w.lin@gmail.com>
2022-12-20 10:45:44 +0800

    hack3

--- hack3/frontend/src/components/Row.js
+++ hack3/frontend/src/components/Row.js
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react';
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 
 import TableCell from '@mui/material/TableCell';
@@ -14,106 +14,107 @@ import dayjs from '../utils/day';
 import ItemFormModal from './ItemFormModal';
 
 function Row({
-  item, updateItem, deleteItem,
-}) {
-  const [descriptionOpen, setDescriptionOpen] = useState(false);
-  const [editOpen, setEditOpen] = useState(false);
+                 item, updateItem, deleteItem,
+             }) {
+    const [descriptionOpen, setDescriptionOpen] = useState(false);
+    const [editOpen, setEditOpen] = useState(false);
 
-  const onCollapse = () => {
-    setDescriptionOpen((open) => !open);
-  };
+    const onCollapse = () => {
+        setDescriptionOpen((open) => !open);
+    };
 
-  const onEdit = () => {
-    setEditOpen((open) => !open);
-  };
+    const onEdit = () => {
+        setEditOpen((open) => !open);
+    };
 
-  const handleDelete = () => {
-    console.log("deleteItem item.id =", item.id)
-    deleteItem({
-      variables: {
-        id: item.id,
-      },
-      onError: (err) => {
-        // eslint-disable-next-line no-console
-        console.error("err",err);
-      },
-    });
-  };
+    const handleDelete = () => {
+        console.log("deleteItem item.id =", item.id)
+        deleteItem({
+            variables: {
+                id: item.id,
+            },
+            onError: (err) => {
+                // eslint-disable-next-line no-console
+                console.error("err", err);
+            },
+        });
+    };
 
-  const handleSubmitEdit = (formData) => {
-    console.log("formData:", formData)
-    updateItem({
-      variables: {
-        // TODO 4 Use `updateItem` and pass the correct variables
-        input:{...formData, id:item.id},
-      },
-        // TODO End
-        onError: (err) => {
-          // eslint-disable-next-line no-console
-          console.error(err);
-        },
+    const handleSubmitEdit = (formData) => {
+        console.log("formData:", formData)
+        updateItem({
+            variables: {
+                // TODO 4 Use `updateItem` and pass the correct variables
+                input: {...formData, id: item.id},
+            },
+            // TODO End
+            onError: (err) => {
+                // eslint-disable-next-line no-console
+                console.error(err);
+            },
 
-    });
-  };
+        });
+    };
 
-  return (
-    <>
-      <TableRow data-cy="item" key={item.id} hover>
-        <TableCell onClick={onCollapse} sx={{ cursor: 'pointer' }}>
-          <Typography>{item.date && dayjs(item.date).calendar()}</Typography>
-        </TableCell>
-        <TableCell data-cy="item-name" onClick={onCollapse} sx={{ cursor: 'pointer' }}>
-          <Typography>{item.name}</Typography>
-        </TableCell>
-        <TableCell data-cy="item-amount" align="right">
-          <Typography>{item.amount && `$${item.amount}`}</Typography>
-        </TableCell>
-        <TableCell data-cy="item-category" onClick={onCollapse} sx={{ cursor: 'pointer' }}>
-          <Typography>{item.category?.toLowerCase()}</Typography>
-        </TableCell>
-        <TableCell align="right" data-cy="item-edit">
-          <IconButton onClick={onEdit} data-cy="update-item">
-            <EditIcon />
-          </IconButton>
-          <IconButton onClick={handleDelete} data-cy="delete-item">
-            <DeleteIcon />
-          </IconButton>
-        </TableCell>
-      </TableRow>
-      <TableRow key={`${item.id}-descriptions`}>
-        <TableCell colSpan={5} style={{ paddingTop: 0, paddingBottom: 0 }}>
-          <Collapse in={descriptionOpen} timeout="auto" unmountOnExit>
-            <div className="p-4">
-              <Typography gutterBottom>
-                Descriptions
-              </Typography>
-              <Typography variant="subtitle2" sx={{ textIndent: '1rem' }}>{item.description || 'No description'}</Typography>
-            </div>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-      <ItemFormModal
-        title="Edit Item"
-        open={editOpen}
-        handleClose={() => setEditOpen(false)}
-        onSubmit={handleSubmitEdit}
-        defaultFormData={item}
-      />
-    </>
-  );
+    return (
+        <>
+            <TableRow data-cy="item" key={item.id} hover>
+                <TableCell onClick={onCollapse} sx={{cursor: 'pointer'}}>
+                    <Typography>{item.date && dayjs(item.date).calendar()}</Typography>
+                </TableCell>
+                <TableCell data-cy="item-name" onClick={onCollapse} sx={{cursor: 'pointer'}}>
+                    <Typography>{item.name}</Typography>
+                </TableCell>
+                <TableCell data-cy="item-amount" align="right">
+                    <Typography>{item.amount && `$${item.amount}`}</Typography>
+                </TableCell>
+                <TableCell data-cy="item-category" onClick={onCollapse} sx={{cursor: 'pointer'}}>
+                    <Typography>{item.category?.toLowerCase()}</Typography>
+                </TableCell>
+                <TableCell align="right" data-cy="item-edit">
+                    <IconButton onClick={onEdit} data-cy="update-item">
+                        <EditIcon/>
+                    </IconButton>
+                    <IconButton onClick={handleDelete} data-cy="delete-item">
+                        <DeleteIcon/>
+                    </IconButton>
+                </TableCell>
+            </TableRow>
+            <TableRow key={`${item.id}-descriptions`}>
+                <TableCell colSpan={5} style={{paddingTop: 0, paddingBottom: 0}}>
+                    <Collapse in={descriptionOpen} timeout="auto" unmountOnExit>
+                        <div className="p-4">
+                            <Typography gutterBottom>
+                                Descriptions
+                            </Typography>
+                            <Typography variant="subtitle2"
+                                        sx={{textIndent: '1rem'}}>{item.description || 'No description'}</Typography>
+                        </div>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+            <ItemFormModal
+                title="Edit Item"
+                open={editOpen}
+                handleClose={() => setEditOpen(false)}
+                onSubmit={handleSubmitEdit}
+                defaultFormData={item}
+            />
+        </>
+    );
 }
 
 Row.propTypes = {
-  item: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    amount: PropTypes.number.isRequired,
-    category: PropTypes.string.isRequired,
-    date: PropTypes.number.isRequired,
-    description: PropTypes.string,
-  }).isRequired,
-  updateItem: PropTypes.func.isRequired,
-  deleteItem: PropTypes.func.isRequired,
+    item: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        amount: PropTypes.number.isRequired,
+        category: PropTypes.string.isRequired,
+        date: PropTypes.number.isRequired,
+        description: PropTypes.string,
+    }).isRequired,
+    updateItem: PropTypes.func.isRequired,
+    deleteItem: PropTypes.func.isRequired,
 };
 
 export default Row;
--- hack3/frontend/src/graphql/mutations.js
+++ hack3/frontend/src/graphql/mutations.js
@@ -1,25 +1,25 @@
-import { gql } from '@apollo/client';
+import {gql} from '@apollo/client';
 
 // TODO 3.1 Mutation - Update item
 export const CREATE_ITEM_MUTATION = gql`
-  mutation CreateItem($input: CreateItemInput!) {
-    createItem(input: $input) {
-      id
+    mutation CreateItem($input: CreateItemInput!) {
+        createItem(input: $input) {
+            id
+        }
     }
-  }
 `;
 // TODO 3.1 End
 
 export const UPDATE_ITEM_MUTATION = gql`
-  mutation UpdateItem($input: UpdateItemInput!) {
-    updateItem(input: $input) {
-      id
+    mutation UpdateItem($input: UpdateItemInput!) {
+        updateItem(input: $input) {
+            id
+        }
     }
-  }
 `;
 
 export const DELETE_ITEM_MUTATION = gql`
-  mutation DeleteItem($id: ID!) {
-    deleteItem(id: $id)
-  }
+    mutation DeleteItem($id: ID!) {
+        deleteItem(id: $id)
+    }
 `;
