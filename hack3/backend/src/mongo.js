d841a3aebba6b48306e5da2fbab0351487ceca49
weitude <eric.c.w.lin@gmail.com>
2022-12-20 10:37:06 +0800

    hack3

--- hack3/backend/src/mongo.js
+++ hack3/backend/src/mongo.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import { dataInit } from "./upload.js";
+import {dataInit} from "./upload.js";
 
 import "dotenv-defaults/config.js";
 
 mongoose.set("strictQuery", true);
 
 async function connect() {
-  // TODO 1 Connect to your MongoDB and call dataInit()
-  //   dotenv.config();
+    // TODO 1 Connect to your MongoDB and call dataInit()
+    //   dotenv.config();
     mongoose.set('strictQuery', false);
 
     if (!process.env.MONGO_URL) {
@@ -15,15 +15,13 @@ async function connect() {
         process.exit(1);
     }
     mongoose
-        .connect(process.env.MONGO_URL, {
-            dbName: "Hack3",
-        })
+        .connect(process.env.MONGO_URL, {})
         .then(() => console.log("Mongo db connection created!"));
     mongoose.connection.on('error',
         console.error.bind(console, 'connection error:'));
 
-  // TODO 1 End
+    // TODO 1 End
     await dataInit()
 }
 
-export default { connect };
+export default {connect};
